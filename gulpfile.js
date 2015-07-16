5af1b4a1e3bbdab2394beb8207eac569baa90319
Oyvind Hoie <hoie@draft.no>
2015-07-16 21:03:22 +0200

    Revert 7f14be3..5c03361
    This rolls back to commit 7f14be34b7ab194df06025c8a2d29a5d3cbf9ded.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,9 @@
 var gulp = require('gulp')
-var flatten = require('gulp-flatten')
 
 gulp.task('html', function(){
 	return gulp
 	.src('*.html')
 	.pipe(gulp.dest('dist'));
-});
-
-gulp.task('fonts', function(){
-	return gulp
-	.src('bower_components/**/*.{eof,svg,ttf,woff,woff2,otf}')
-	.pipe(flatten())
-	.pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('assets', function(){
